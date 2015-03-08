6cc7890b84b626d62b86d2d5b7d9df17575d0df8
ozcarzarate <oscar.zarate@gmail.com>
2015-03-08 20:00:16 +1100

    blah

--- ExpressApp1/app.js
+++ ExpressApp1/app.js
@@ -29,7 +29,6 @@ app.use('/users', users);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
-    var something = 1;
     err.status = 404;
     next(err);
 });
